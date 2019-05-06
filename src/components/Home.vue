e1258dfe7a530dcef57548bc25ab7084aa05018a
Izaya Orihara <izaya.san@outlook.com>
2019-05-06 17:46:36 +0800

    Add special effect and refinement

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,39 +1,49 @@
 <template>
   <div>
     <div>
-      <b-button-group v-for="board in boards">
-        <b-button variant="link" @click="changeBoard(board.path)">{{ board.name }}</b-button>
-      </b-button-group>
+      <b-nav tabs fill>
+        <b-nav-item variant="link" @click="changeBoard(board.path)" v-for="board in boards">{{ board.name }}</b-nav-item>
+      </b-nav>
     </div>
-    <b-card>
-      <div class="container">
-        <div class="item-view" v-for="(post, index) in posts" v-show="show">
-          <div class="item-view-header">
-            <a @click="getComments(index)"><h1>{{ post.content }}</h1></a>
+    <b-card-group deck>
+      <b-card class="card card__one" v-for="(post, index) in posts" v-show="show" @click="getComments(index)">
+        <b-card-title>{{ post.content }}</b-card-title>
+        <b-card-sub-title><p class="meta">
+          {{ post.createtime }} | {{ post.username }}
+        </p></b-card-sub-title>
+      </b-card>
+      <b-card v-show="!show">
+        <b-card-title>
+          {{ posts[this.detail_index].content }}
+        </b-card-title>
+        <p class="meta">
+          {{ this.posts[this.detail_index].createtime }} | {{ this.posts[this.detail_index].username }}
+        </p>
+        <b-card-group deck>
+          <b-card class="item-view-header" v-show="!show" v-for="(comment, index) in comments">
+            <b-card-title>{{ comment.content }}</b-card-title>
             <p class="meta">
-              {{ post.createtime }} | {{ getUsername(post.uid) }}
+              {{ comment.createtime }} | {{ comment.username }}
             </p>
-          </div>
-        </div>
-        <div class="item-view">
-          <div class="item-view-header" v-show="!show" v-for="(comment, index) in comments">
-            <h2 style="font-size:12px">{{ comment.content }}</h2>
-            <p class="meta">
-              {{ comment.createtime }}
-            </p>
-          </div>
-        </div>
+          </b-card>
+        </b-card-group>
         <div>
-          <b-button v-if="!show" variant="link" @click="onReturn">return</b-button>
+          <p></p>
+          <b-button v-if="!show" variant="primary" @click="onReturn">return</b-button>
         </div>
+      </b-card>
+    </b-card-group>
+    <div>
+      <br>
+    </div>
 
-      </div>
-    </b-card>
   </div>
 </template>
 
 <script>
   import axios from 'axios';
+  import TimeAgo from 'javascript-time-ago';
+  import en from 'javascript-time-ago/locale/en';
   import app from '../App';
 
   export default {
@@ -72,34 +82,15 @@
           path: 'index',
         },
         show: true,
+        detail_index: 0,
       };
     },
     created() {
-      axios.post(`${app.baseURL}api/getposts/`, JSON.stringify(this.panel)).then(
-        (response) => {
-          this.response = response;
-          if (response.data === 'U200') {
-            // 清除cookies
-            this.$router.push('/');
-          } else {
-            this.posts = response.data.posts;
-          }
-        },
-      ).catch();
-      axios.get(`${app.baseURL}api/getboards`).then(
-        (response) => {
-          console.log(response);
-          if (response.data === 'U200') {
-            this.$router.push('/');
-          } else {
-            this.boards = response.data.board;
-          }
-        },
-      ).catch();
+      this.updatePosts();
+      this.updateBoard();
     },
     methods: {
-      changeBoard(path) {
-        this.panel.path = path;
+      updatePosts() {
         axios.post(`${app.baseURL}api/getposts/`, JSON.stringify(this.panel)).then(
           (response) => {
             this.response = response;
@@ -108,23 +99,40 @@
               this.$router.push('/');
             } else {
               this.posts = response.data.posts;
+              this.convertTimeAgo();
             }
           },
         ).catch();
       },
-      getUsername(uid_) {
-        console.log(uid_);
-        axios.post(`${app.baseURL}api/getuserinfo/`, JSON.stringify({ uid: uid_ })).then(
+      updateBoard() {
+        axios.get(`${app.baseURL}api/getboards`).then(
           (response) => {
-            if (response.data === 'G104') {
-              return 'Anonymous';
+            // eslint-disable-next-line no-console
+            console.log(response);
+            if (response.data === 'U200') {
+              this.$router.push('/');
+            } else {
+              this.boards = response.data.board;
             }
-            return response.data.username;
           },
         ).catch();
+      },
+      convertTimeAgo() {
+        const postLen = this.posts.length;
+        TimeAgo.addLocale(en);
+        for (let i = 0; i < postLen; i += 1) {
+          const current = new TimeAgo('en-US');
+          this.posts[i].createtime = current.format(Date.parse(this.posts[i].createtime));
+        }
+      },
+      changeBoard(path) {
+        this.panel.path = path;
+        this.updatePosts();
+        this.show = true;
       },
       onReturn(evt) {
         evt.preventDefault();
+        // eslint-disable-next-line no-console
         console.log(evt);
         this.show = !this.show;
         console.log(this.show);
@@ -132,6 +140,7 @@
       getComments(i) {
         console.log(i);
         this.show = false;
+        this.detail_index = i;
         axios.post(`${app.baseURL}api/getresponse/`, JSON.stringify(this.posts[i])).then(
           (response) => {
             console.log(response);
@@ -148,56 +157,138 @@
 </script>
 
 <style scoped>
-  .item-view-header {
-    background-color: #fff;
-    padding: 1.8em 2em 1em;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, .1);
+  body {
+    display: flex;
+    align-items: center;
+    min-height: 100vh;
+    background: #f8f8f8;
   }
-    h1 {
-      text-decoration-style: solid;
-      text-decoration-color: #527394;
-      line-height: 20px;
-      display: inline;
-      font-size: 16px;
-      margin: 0 .5em 0 0;
-      font-weight: 300;
-
-
+  .grid {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    max-width: 340px;
+    margin: 0 auto;
+  }
+  @media screen and (min-width: 768px) {
+    .grid {
+      max-width: 1200px;
     }
-    .host, .meta, .meta a {
-      color: #828282;
-      font-size: 12px;
-      text-align: left;
+  }
+  .card {
+    position: relative;
+    flex: 1 1 100%;
+    margin: 31px 0;
+    padding: 20px;
+    background: white;
+  }
+  @media screen and (min-width: 768px) {
+    .card {
+      flex-basis: calc(33.33% - (62px + 40px));
+      margin: 0 31px;
     }
-    .meta a {
-      text-decoration: underline
-    }
-
-    .item-view-comments {
-      background-color: #fff;
-      margin-top: 10px;
-      padding: 0 2em .5em
-    }
-
-    .item-view-comments-header{
-    margin: 0;
-    font-size: 1.1em;
-    padding: 1em 0;
-    position: relative
-    }
-    .spinner{
-    display: inline-block;
-    margin: -15px 0;}
-
-    .comment-children{
-    list-style-type: none;
-    padding: 0;
-    margin: 0}
-
-    @media (max-width: 600px) {
-      .item-view-header
-      h1 {
-        font-size: 1.25em
-      }
-    }
+  }
+  .card__thumb {
+    overflow: hidden;
+  }
+  .card__img {
+    margin: -20px -20px 0;
+  }
+  .card__img img {
+    max-width: 100%;
+    height: auto;
+    border: 0;
+    vertical-align: middle;
+    box-sizing: border-box;
+  }
+  .card__desc {
+    margin-top: 20px;
+  }
+  .card__two .card__img {
+    padding: 20px;
+  }
+  .card__one {
+    transition: transform .5s;
+  }
+  .card__one::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 2s cubic-bezier(0.165, 0.84, 0.44, 1);
+    box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.15);
+    content: '';
+    opacity: 0;
+    z-index: -1;
+  }
+  .card__one:hover,
+  .card__one:focus {
+    transform: scale3d(1.006, 1.006, 1);
+  }
+  .card__one:hover::after,
+  .card__one:focus::after {
+    opacity: 1;
+  }
+  .card__two::before,
+  .card__two::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    transition: opacity .38s ease-in-out, transform .35s ease-in-out;
+    content: '';
+    opacity: 0;
+    pointer-events: none;
+  }
+  .card__two::before {
+    transform: scale3d(0, 1, 1);
+    transform-origin: left top;
+    border-top: 1px solid #acacac;
+    border-bottom: 1px solid #acacac;
+  }
+  .card__two::after {
+    transform: scale3d(1, 0, 1);
+    transform-origin: right top;
+    border-right: 1px solid #acacac;
+    border-left: 1px solid #acacac;
+  }
+  .card__two:hover::before,
+  .card__two:hover::after,
+  .card__two:focus::before,
+  .card__two:focus::after {
+    transform: scale3d(1, 1, 1);
+    opacity: 1;
+  }
+  .card__three::before,
+  .card__three::after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    transform: scale3d(0, 0, 1);
+    transition: transform .3s ease-out 0s;
+    background: rgba(255, 255, 255, 0.1);
+    content: '';
+    pointer-events: none;
+  }
+  .card__three::before {
+    transform-origin: left top;
+  }
+  .card__three::after {
+    transform-origin: right bottom;
+  }
+  .card__three:hover::before,
+  .card__three:hover::after,
+  .card__three:focus::before,
+  .card__three:focus::after {
+    transform: scale3d(1, 1, 1);
+  }
+  .meta{
+    text-align: end;
+    color: #acacac;
+    font-size: small;
+  }
 </style>
